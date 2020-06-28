896b54334a3b5d61bad3bb36f806322144f35e27
Justin <justinknueppel@gmail.com>
2020-06-28 11:29:07 -0400

    Add bst remove

--- Algorithms/bst.ts
+++ Algorithms/bst.ts
@@ -16,4 +16,56 @@ const insert = (root: Node, node: Node): void => {
   }
 };
 
-export { insert };
+const remove = (root: Node, node: Node): Node => {
+  // Find node
+  let curNode = root;
+  while (curNode.val !== node.val) {
+    if (node.val <= curNode.val) {
+      if (curNode.left) {
+        if (curNode.left.val === node.val && !curNode.left.left && !curNode.left.right) {
+          curNode.left = undefined;
+          return root;
+        }
+        curNode = curNode.left;
+      } else {
+        return root;
+      }
+    } else {
+      if (curNode.right) {
+        if (curNode.right.val === node.val && !curNode.right.left && !curNode.right.right) {
+          curNode.right = undefined;
+          return root;
+        }
+        curNode = curNode.right;
+      } else {
+        return root;
+      }
+    }
+  }
+  // Remove node, we know that curNode has at least one child
+  if (!curNode.left) {
+    curNode.val = curNode.right!.val;
+    curNode.left = curNode.right!.left;
+    curNode.right = curNode.right!.right;
+  } else if (!curNode.right) {
+    curNode.val = curNode.left!.val;
+    curNode.right = curNode.left!.right;
+    curNode.left = curNode.left!.left;
+  } else {
+    //Both children exist, need to find and remove smallest node from right child
+    const smallestInRight = findSmallestNode(curNode.right);
+    remove(curNode, smallestInRight)
+    curNode.val = smallestInRight.val;
+  }
+  return root;
+}
+
+const findSmallestNode = (root: Node): Node => {
+  let curNode = root;
+  while (curNode.left) {
+    curNode = curNode.left;
+  }
+  return curNode;
+}
+
+export { insert, remove };
