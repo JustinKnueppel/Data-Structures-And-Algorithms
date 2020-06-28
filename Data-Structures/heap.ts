e1d3d427956220f4b5d9ecea6b140f0b73e1b536
Justin <justinknueppel@gmail.com>
2020-06-28 15:27:39 -0400

    Further reduce code duplication of heaps

--- Data-Structures/heap.ts
+++ Data-Structures/heap.ts
@@ -1,39 +1,11 @@
 abstract class Heap {
   protected values: Array<number>;
   protected heapSize: number;
+
   constructor(values?: Array<number>) {
     this.values = values || new Array<number>();
     this.heapSize = this.values.length;
     this.heapify();
-  }
-
-  private heapify(): void {
-    const end = this.heapSize - 1;
-    let start = this.parent(end);
-    while (start >= 0) {
-      this.siftDown(start--, end);
-    }
-  }
-
-  protected abstract siftDown(start: number, end: number): void;
-  protected abstract bubbleUp(child: number): void;
-
-  protected swap(i: number, j: number): void {
-    const temp = this.values[i];
-    this.values[i] = this.values[j];
-    this.values[j] = temp;
-  }
-
-  protected parent(child: number): number {
-    return Math.ceil(child / 2) - 1;
-  }
-
-  protected left(parent: number): number {
-    return parent * 2 + 1;
-  }
-
-  protected right(parent: number): number {
-    return this.left(parent) + 1;
   }
 
   public add(value: number): void {
@@ -53,57 +25,77 @@ abstract class Heap {
   public size(): number {
     return this.heapSize;
   }
-}
 
-class MaxHeap extends Heap {
-  protected siftDown(start: number, end: number): void {
+  protected abstract leftShouldBeParent(left: number, right: number): boolean;
+
+  private heapify(): void {
+    const end = this.heapSize - 1;
+    let start = this.parent(end);
+    while (start >= 0) {
+      this.siftDown(start--, end);
+    }
+  }
+
+  private siftDown(start: number, end: number): void {
     let root = start;
     while (this.left(root) <= end) {
       let child = this.left(root);
       const right = this.right(root);
-      if (right <= end && this.values[child] < this.values[right]) {
+      if (
+        right <= end &&
+        this.leftShouldBeParent(this.values[right], this.values[child])
+      ) {
         child = right;
       }
 
-      if (this.values[root] > this.values[child]) return;
+      if (this.leftShouldBeParent(this.values[root], this.values[child]))
+        return;
 
       this.swap(root, child);
       root = child;
     }
   }
-  protected bubbleUp(child: number): void {
+
+  private bubbleUp(child: number): void {
     let parent = this.parent(child);
-    while (parent >= 0 && this.values[child] > this.values[parent]) {
+    while (
+      parent >= 0 &&
+      this.leftShouldBeParent(this.values[child], this.values[parent])
+    ) {
       this.swap(parent, child);
       child = parent;
       parent = this.parent(child);
     }
+  }
+
+  private swap(i: number, j: number): void {
+    const temp = this.values[i];
+    this.values[i] = this.values[j];
+    this.values[j] = temp;
+  }
+
+  private parent(child: number): number {
+    return Math.ceil(child / 2) - 1;
+  }
+
+  private left(parent: number): number {
+    return parent * 2 + 1;
+  }
+
+  private right(parent: number): number {
+    return this.left(parent) + 1;
+  }
+}
+
+class MaxHeap extends Heap {
+  protected leftShouldBeParent(left: number, right: number): boolean {
+    return left > right;
   }
 }
 
 class MinHeap extends Heap {
-  protected siftDown(start: number, end: number): void {
-    let root = start;
-    while (this.left(root) <= end) {
-      let child = this.left(root);
-      const right = this.right(root);
-      if (right <= end && this.values[child] > this.values[right]) {
-        child = right;
-      }
-
-      if (this.values[root] < this.values[child]) return;
-
-      this.swap(root, child);
-      root = child;
-    }
-  }
-  protected bubbleUp(child: number): void {
-    let parent = this.parent(child);
-    while (parent >= 0 && this.values[child] < this.values[parent]) {
-      this.swap(parent, child);
-      child = parent;
-      parent = this.parent(child);
-    }
+  protected leftShouldBeParent(left: number, right: number): boolean {
+    return left < right;
   }
 }
 
